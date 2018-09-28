85a16332f5ccc9808688be44c4463f1758afcfa6
Eoghan <eoghan.gallagher@gmail.com>
2018-09-28 18:23:27 +0100

    Initial commit

---
+++ Assets/Asteroid.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour 
+{
+
+	[SerializeField] private GameObject crossHair;
+
+
+	 void OnMouseDown()
+     {
+         print (name); 
+		 ToggleCrosshair();
+
+
+     }
+
+	 private void ToggleCrosshair()
+	 {
+		 if( crossHair.activeSelf )
+		 {
+			 crossHair.SetActive( false );
+		 }
+		 else
+		 {
+			 crossHair.SetActive( true );
+		 }
+	 }
+	
+}
---
+++ Assets/CrossHair.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossHair : MonoBehaviour 
+{
+
+	private Transform _transform;
+	[SerializeField] private Transform cameraTransform;
+
+	private void Start()
+	{
+		_transform = transform;
+	
+	}
+	void Update() 
+    {
+        _transform.forward = cameraTransform.forward;
+    }
+}
